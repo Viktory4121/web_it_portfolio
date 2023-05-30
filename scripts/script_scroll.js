8f5361a8fbe3e3668f1e9caf79d92bbbaf68269d
Viktory4121 <medvedka4121@gmail.com>
2023-05-30 23:34:12 +0300

    add scrolling

---
+++ scripts/script_scroll.js
@@ -0,0 +1,15 @@
+document.getElementById("button-about-me").onclick = function() {
+    document.getElementById("tab-about-me").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("button-projects").onclick = function() {
+    document.getElementById("tab-projects").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("button-work-video").onclick = function() {
+    document.getElementById("tab-work-video").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("button-inputs").onclick = function() {
+    document.getElementById("tab-inputs").scrollIntoView({behavior: "smooth"});
+}
